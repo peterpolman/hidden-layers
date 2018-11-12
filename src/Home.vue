e5453f0750a68cc9ac04231eafd4d919a67b4522
Peter Polman <peter@peterpolman.nl>
2018-11-12 16:21:43 +0100

    Added better signal UX and move styling into separate module

--- src/Home.vue
+++ src/Home.vue
@@ -53,27 +53,57 @@ export default {
 
   button {
     position: absolute;
-    top: 0;
-    left: 0;
-    background: black;
-    color: white;
-    padding: .7rem;
+    bottom: .5rem;
+    left: .5rem;
+    font-size: 10px;
+    width: 80px;
+    border-radius: 5px;
+    width: 60px;
+    height: 30px;
+    padding: 0 5px;
+    background-color: black;
   }
 
   .geo-on,
   .geo-off {
+    position: fixed;
+    top: 1rem;
+    left: 1rem;
+    z-index: 1;
+  }
+
+  .geo-on:before,
+  .geo-off:before {
+    content: "";
     display: block;
     width: 20px;
     height: 20px;
     border-radius: 50%;
     background: #f65858;
-    position: fixed;
-    top: 50px;
-    left: 1rem;
-    z-index: 1;
+    position: absolute;
   }
 
-  .geo-on {
+  .geo-on:before {
     background: #4df04d;
+  }
+
+  .geo-off:after {
+    content: "Tap to enable GPS";
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    position: absolute;
+    left: 20px;
+    margin-left: .5rem;
+
+    background-color: rgba(0,0,0,0.5);
+    color: white;
+    font-weight: bold;
+    font-size: 10px;
+    width: 90px;
+    border-radius: 5px;
+    height: 20px;
+    padding: 0 5px;
   }
 </style>
--- src/services/MapService.js
+++ src/services/MapService.js
@@ -6,6 +6,7 @@ import MarkerService from './MarkerService';
 import UserService from './UserService';
 
 import config from '../config.js'
+import MapStyles from '../mapStyles.js'
 
 export default class MapService {
   constructor() {
@@ -14,9 +15,9 @@ export default class MapService {
     this.markerService = new MarkerService
     this.userService = new UserService
     this.loader = GoogleMapsLoader
+    this.mapStyles = MapStyles
   }
 
-  // Style the map with https://mapstyle.withgoogle.com/
   init() {
     this.loader.KEY = config.maps.key;
     this.loader.LIBRARIES = ['geometry'];
@@ -32,7 +33,7 @@ export default class MapService {
         mapTypeControl: false,
         scrollwheel: true,
         streetViewControl: false,
-        styles: [{"elementType":"geometry","stylers":[{"color":"#ebe3cd"}]},{"elementType":"labels","stylers":[{"visibility":"off"}]},{"elementType":"labels.text.fill","stylers":[{"color":"#523735"}]},{"elementType":"labels.text.stroke","stylers":[{"color":"#f5f1e6"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#c9b2a6"}]},{"featureType":"administrative.land_parcel","stylers":[{"visibility":"off"}]},{"featureType":"administrative.land_parcel","elementType":"geometry.stroke","stylers":[{"color":"#dcd2be"}]},{"featureType":"administrative.land_parcel","elementType":"labels.text.fill","stylers":[{"color":"#ae9e90"}]},{"featureType":"administrative.neighborhood","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"color":"#dfd2ae"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#dfd2ae"}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"color":"#93817c"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#a5b076"}]},{"featureType":"poi.park","elementType":"labels.text.fill","stylers":[{"color":"#447530"}]},{"featureType":"road","elementType":"geometry","stylers":[{"color":"#f5f1e6"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#fdfcf8"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#f8c967"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#e9bc62"}]},{"featureType":"road.highway.controlled_access","elementType":"geometry","stylers":[{"color":"#e98d58"}]},{"featureType":"road.highway.controlled_access","elementType":"geometry.stroke","stylers":[{"color":"#db8555"}]},{"featureType":"road.local","elementType":"labels.text.fill","stylers":[{"color":"#806b63"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"color":"#dfd2ae"}]},{"featureType":"transit.line","elementType":"labels.text.fill","stylers":[{"color":"#8f7d77"}]},{"featureType":"transit.line","elementType":"labels.text.stroke","stylers":[{"color":"#ebe3cd"}]},{"featureType":"transit.station","elementType":"geometry","stylers":[{"color":"#dfd2ae"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#b9d3c2"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#92998d"}]}]
+        styles: this.mapStyles
       }
 
       this.map = new google.maps.Map(element, options)
