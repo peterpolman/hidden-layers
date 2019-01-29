7493b3029f41c96a500a771a02a7f3bca6a720b6
Peter Polman <peter@peterpolman.nl>
2019-01-29 15:58:39 +0100

    Prepared public transpart traveling feature for places

--- src/controllers/StoreController.js
+++ src/controllers/StoreController.js
@@ -26,57 +26,65 @@ export default class StoreController {
 		} else {
 			new google.maps.places.PlacesService(MAP).getDetails({ placeId: e.placeId }, (place, status) => {
 				if (status === google.maps.places.PlacesServiceStatus.OK) {
-                    const availableItems = {
-						ward: {
-							slug: 'ward',
-							name: 'Ward',
-                            size: 40,
-                            amount: (Math.floor(Math.random() * 100) > 90) ? 1 : 0,
-						},
-						gold: {
-							slug: 'gold',
-							name: 'Gold',
-                            size: 25,
-							amount: Math.floor(Math.random() * 20),
-						},
-						sword: {
-							slug: 'sword',
-							name: 'Wooden Sword',
-                            size: 25,
-							amount: (Math.floor(Math.random() * 100) > 90) ? 1 : 0,
-						},
-						discover: {
-							slug: 'discover',
-							name: 'Map',
-                            size: 30,
-							amount: (Math.floor(Math.random() * 100) > 90) ? 1 : 0,
-						},
-                        potion: {
-							slug: 'potion',
-							name: 'Potion',
-                            size: 25,
-							amount: (Math.floor(Math.random() * 100) > 90) ? 1 : 0,
-						}
-					}
-
-					this.storesRef.child(e.placeId).set({
-						id: e.placeId,
-						owner: this.uid,
-						name: place.name,
-						category: place.types[0],
-						position: {
-							lat: place.geometry.location.lat(),
-							lng: place.geometry.location.lng()
-						},
-						items: availableItems
-					})
-
-					this.store = e.placeId
-
+                    if (place.types[0] === 'light_rail_station' || place.types[0] === 'transit_station' || place.types[0] === 'train_station' || place.types[0] === 'subway_station') {
+                        alert('Soon you will be able to travel great distances into new adventures!')
+                    }
+                    else {
+                        this.visitStore(place)
+                    }
 				}
 			})
 		}
 	}
+
+    visitStore(place) {
+        const availableItems = {
+            ward: {
+                slug: 'ward',
+                name: 'Ward',
+                size: 40,
+                amount: (Math.floor(Math.random() * 100) > 90) ? 1 : 0,
+            },
+            gold: {
+                slug: 'gold',
+                name: 'Gold',
+                size: 25,
+                amount: Math.floor(Math.random() * 20),
+            },
+            sword: {
+                slug: 'sword',
+                name: 'Wooden Sword',
+                size: 25,
+                amount: (Math.floor(Math.random() * 100) > 90) ? 1 : 0,
+            },
+            discover: {
+                slug: 'discover',
+                name: 'Map',
+                size: 30,
+                amount: (Math.floor(Math.random() * 100) > 90) ? 1 : 0,
+            },
+            potion: {
+                slug: 'potion',
+                name: 'Potion',
+                size: 25,
+                amount: (Math.floor(Math.random() * 100) > 90) ? 1 : 0,
+            }
+        }
+
+        this.storesRef.child(place.place_id).set({
+            owner: this.uid,
+            id: place.place_id,
+            name: place.name,
+            category: place.types[0],
+            position: {
+                lat: place.geometry.location.lat(),
+                lng: place.geometry.location.lng()
+            },
+            items: availableItems
+        })
+
+        this.store = place.place_id
+    }
 
     onStoreAdded(id, data) {
 		const randInt = Math.floor(Math.random() * 100)
