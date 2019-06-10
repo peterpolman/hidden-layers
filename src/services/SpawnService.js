fdce2d3e42066e86c71e863d57637083ef285936
Peter Polman <peter@peterpolman.nl>
2019-06-10 17:09:41 +0200

    Started on generating new positions for enemies

--- src/services/SpawnService.js
+++ src/services/SpawnService.js
@@ -1,3 +1,4 @@
+const Geohash = require('latlon-geohash');
 // import firebase from 'firebase/app';
 
 export default class SpawnService {
@@ -14,14 +15,25 @@ export default class SpawnService {
             HL.scout.position
         ]
         // Loop through the positions that have discovery
-        for (let id in positions) {
+        for (let i in positions) {
             // Calculate the chance to spawn an enemy for all of them
             const p = Math.random(0,1);
             const pRate = .1;
             if (p < pRate) {
-                console.log('Spawn at', id)
+                let hash = Geohash.encode(positions[i].lat, positions[i].lng, 7);
+                var bounds = Geohash.bounds(hash);
+                console.log(bounds);
+                console.log('Spawn enemy near ', i)
                 // Create an enemy
+                var x_max = bounds.ne.lat;
+                var x_min = bounds.sw.lat;
+                var y_max = bounds.ne.lon;
+                var y_min = bounds.sw.lon;
 
+                var lat = y_min + (Math.random() * (y_max - y_min));
+                var lng = x_min + (Math.random() * (x_max - x_min));
+
+                console.log(lat, lng);
                 // Push it in the enemy database
 
                 // Push it in the marker geohash for the current uid
