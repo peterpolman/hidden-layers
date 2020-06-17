314c6b65c7769284dac5bff3b32510fa6dc2ad9d
Peter Polman <peter@peterpolman.nl>
2020-06-17 09:53:54 +0200

    Refactors timer a bit

--- src/components/BaseAction.ts
+++ src/components/BaseAction.ts
@@ -44,6 +44,8 @@ export default class BaseAction extends Vue {
     combatTimer: any;
     attacking = false;
     consuming = false;
+    time = 0;
+    actionTimer: any;
 
     onMainClick() {
         this.main.activate();
@@ -63,58 +65,61 @@ export default class BaseAction extends Vue {
         }
     }
 
-    timer: any;
-    time = 0;
-
     async attack(weapon: Weapon, target: any) {
-        this.attacking = true;
-        this.timer = window.setInterval(() => {
-            this.time += weapon.speed / 100;
+        const die = async (t: any) => {
+            await this.$store.dispatch('markers/remove', target);
+            await t.ref.remove();
+        };
+        const dropLoot = async (t: any) => {
+            const snap = await firebase.db.ref(`items`).once('value');
+            const keys = Object.keys(snap.val());
+            const index = Math.floor(Math.random() * keys.length);
 
-            if (this.time > weapon.speed) {
-                window.clearInterval(this.timer);
-                this.time = 0;
-                console.log('cleared');
+            // Always drop some coin
+            await this.$store.dispatch('inventory/place', {
+                position: t.position,
+                item: new Item({
+                    amount: Math.floor(Math.random() * 20) + 5,
+                    id: '-M8fjDoqeLMlz6xGrM-C',
+                }),
+            });
+
+            // Drop rate item 25%
+            if (Math.random() < 0.25) {
+                await this.$store.dispatch('inventory/place', {
+                    position: t.position,
+                    item: new Item({
+                        amount: 1,
+                        id: keys[index],
+                    }),
+                });
             }
-        }, 1);
-        this.combatTimer = window.setTimeout(async () => {
-            if (target.ref) {
-                const damage = weapon.damage + Math.floor(Math.random() * 10);
-                const hp = target.hitPoints - damage;
-                if (hp > 0) {
-                    await target.ref.update({ hitPoints: hp });
-                } else {
-                    this.$store.commit('markers/deselect');
+        };
 
-                    await this.$store.dispatch('markers/remove', target);
-                    await target.ref.remove();
+        if (target.ref) {
+            this.attacking = true;
+            this.actionTimer = window.setInterval(async () => {
+                this.time += weapon.speed / 100;
 
-                    await this.$store.dispatch('inventory/place', {
-                        position: target.position,
-                        item: new Item({
-                            amount: Math.floor(Math.random() * 20) + 5,
-                            id: '-M8fjDoqeLMlz6xGrM-C',
-                        }),
-                    });
+                if (this.time >= weapon.speed) {
+                    const damage = weapon.damage + Math.floor(Math.random() * 10);
+                    const hp = target.hitPoints - damage;
 
-                    if (Math.random() < 0.25) {
-                        const snap = await firebase.db.ref(`items`).once('value');
-                        const keys = Object.keys(snap.val());
-                        const index = Math.floor(Math.random() * keys.length);
+                    window.clearInterval(this.actionTimer);
 
-                        await this.$store.dispatch('inventory/place', {
-                            position: target.position,
-                            item: new Item({
-                                amount: 1,
-                                id: keys[index],
-                            }),
-                        });
+                    if (hp < 0) {
+                        this.$store.commit('markers/deselect');
+
+                        dropLoot(target);
+                        die(target);
+                    } else {
+                        await target.ref.update({ hitPoints: hp });
                     }
+                    this.attacking = false;
+                    this.time = 0;
                 }
-            }
-            window.clearTimeout(this.combatTimer);
-            this.attacking = false;
-        }, weapon.speed);
+            }, 1);
+        }
     }
 
     consume(item: Consumable, target: any) {
